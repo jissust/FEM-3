8c0bd644e7de851239cd3e5050cfc5d7dc613ae5
jesustissera <jesustissera@gmail.com>
2025-04-17 18:20:46 -0300

    basic structure: I get data from the json file

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -1,8 +1,24 @@
-function Grid () {
-    return (
-        <section className="max-w-[1170px] mx-auto">
-            Section Grid
-        </section>
-    )
+import data from "../../data/data.json";
+
+function Grid() {
+  return (
+    <section className="max-w-[1170px] mx-auto">
+      {data.map((item) => (
+        <div>
+          <div className="flex">
+            <div>Logo</div>
+            <div>
+              <div>{item.name}</div>
+              <div>{item.description}</div>
+            </div>
+          </div>
+          <div className="flex">
+            <button>remove</button>
+            <div>button</div>
+          </div>
+        </div>
+      ))}
+    </section>
+  );
 }
 export default Grid;
