1fc337840ee799151c25b55056025632c2c2e510
jesustissera <jesustissera@gmail.com>
2025-04-17 19:35:59 -0300

    Grid: operation starting with 3 elements per row and responsive versions.

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -3,21 +3,23 @@ import data from "../../data/data.json";
 function Grid() {
   return (
     <section className="max-w-[1170px] mx-auto">
-      {data.map((item) => (
-        <div>
-          <div className="flex">
-            <div>Logo</div>
-            <div>
-              <div>{item.name}</div>
-              <div>{item.description}</div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5">
+        {data.map((item) => (
+          <div>
+            <div className="flex">
+              <div>Logo</div>
+              <div>
+                <div>{item.name}</div>
+                <div>{item.description}</div>
+              </div>
+            </div>
+            <div className="flex">
+              <button>remove</button>
+              <div>button</div>
             </div>
           </div>
-          <div className="flex">
-            <button>remove</button>
-            <div>button</div>
-          </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </section>
   );
 }
