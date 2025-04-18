0ffb86b5d7155e920a63b682bec63613461a07d2
jesustissera <jesustissera@gmail.com>
2025-04-18 02:11:45 -0300

    first changes for dark mode

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -1,25 +1,49 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import dataJson from "../../data/data.json";
 
-function Grid() {
-  const [data, setData] = useState(dataJson);
+const LOCAL_STORAGE_KEY = "extensionsData";
 
+function Grid() {
+  const [data, setData] = useState(() => {
+    const savedData = localStorage.getItem(LOCAL_STORAGE_KEY);
+    return savedData ? JSON.parse(savedData) : dataJson;
+  });
+
+  const [dataFilter, setDataFilter] = useState(data)
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(data));
+  }, [data]);
+  
   const filterDataIsActive = () => {
-    const activeItems = dataJson.filter((item) => item.isActive);
-    setData(activeItems);
+    const activeItems = data.filter((item) => item.isActive);
+    setDataFilter(activeItems);
   };
 
   const filterDataInactive = () => {
-    const activeItems = dataJson.filter((item) => !item.isActive);
-    setData(activeItems);
+    const activeItems = data.filter((item) => !item.isActive);
+    setDataFilter(activeItems);
   };
 
-  const resetFilter = () => setData(dataJson);
+  const resetFilter = () => {
+    setData(data)
+    setDataFilter(data);
+  };
 
-  const removeItem = (index) => {
+  const removeItem = (index: string) => {
     const updatedItems = data.filter((item) => item.name != index);
-    setData(updatedItems)
-  }
+    setData(updatedItems);
+    setDataFilter(updatedItems);
+  };
+
+  const changeState = (index: string) => {
+    const updatedItems = data.map((item) =>
+      item.name === index ? { ...item, isActive: !item.isActive } : item
+    );
+    setData(updatedItems);
+    setDataFilter(updatedItems);
+  };
+
   return (
     <>
       <section className="max-w-[1170px] mx-auto flex justify-between">
@@ -34,20 +58,32 @@ function Grid() {
       </section>
       <section className="max-w-[1170px] mx-auto">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5">
-          {data.map((item) => (
+          {dataFilter.map((item) => (
             <div key={item.name}>
               <div className="flex">
                 <div>
                   <img src={item.logo} alt={item.name} />
                 </div>
-                <div>
+                <div className="bg-white dark:bg-black">
                   <div>{item.name}</div>
                   <div>{item.description}</div>
                 </div>
               </div>
               <div className="flex">
-                <button onClick={() => {removeItem(item.name)}}>remove</button>
-                <div>button</div>
+                <button
+                  onClick={() => {
+                    removeItem(item.name);
+                  }}
+                >
+                  remove
+                </button>
+                <div>
+                  <input
+                    type="checkbox"
+                    checked={item.isActive}
+                    onChange={() => changeState(item.name)}
+                  />
+                </div>
               </div>
             </div>
           ))}
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,19 @@
+import React, { useState } from "react";
 import { Logo } from "../Icons/Icons";
-import { IconMoon } from "../Icons/Icons";
+import { IconMoon, IconSun } from "../Icons/Icons";
 
 function Header() {
+  const [themeDark, setThemeDark] = useState(false)
+  const changeTheme = () => {
+    setThemeDark(!themeDark)
+  }
   return (
     <nav>
       <div className="max-w-[1170px] mx-auto flex justify-between items-center bg-white p-[15px] rounded-3xl">
         <Logo />
-        <div>
-          <IconMoon />
-        </div>
+        <button onClick={changeTheme}>
+          { themeDark ?  <IconSun /> : <IconMoon /> }
+        </button>
       </div>
     </nav>
   );
