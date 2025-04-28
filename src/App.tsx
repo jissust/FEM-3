86b2a425402c5e6fc3470b56025805fd620f80b6
jesustissera <jesustissera@gmail.com>
2025-04-27 23:16:13 -0300

    arreglo color de fondo

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Grid from './components/Grid/Grid'
 function App() {
 
   return (
-    <main className='py-[19px] md:py-[42px] bg-[#eef8fa] dark:bg-gradient-to-br from-[#050A1D] to-[#0A1640] px-[17px] xl:px-0'>
+    <main className='py-[19px] md:py-[42px] bg-gradient-to-br from-[#ECF3FD] to-[#EFFBF9] dark:from-[#050A1D] dark:to-[#0A1640] px-[17px] xl:px-0'>
       <Header />
       <Grid />
     </main>
