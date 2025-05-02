ce074ebe019e22a3e85cca0bd5585d644ff9f78f
jesustissera <jesustissera@gmail.com>
2025-05-02 01:36:44 -0300

    never used

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Logo, LogoDark } from "../Icons/Icons";
 import { IconMoon, IconSun } from "../Icons/Icons";
 
